e67bdd466e4f7da27caeb2b43ee317f8fab249ba
Peer Juergens <peerjuergens2000@gmail.com>
2025-04-08 12:14:06 +0000

    initial commit

---
+++ src/.ipynb_checkpoints/classes-checkpoint.py
@@ -0,0 +1,61 @@
+from dataclasses import dataclass
+from typing import Union
+import numpy as np
+
+
+@dataclass
+class TankProperties32x2:
+    T_d: int = 194
+    T_r: int = 97
+    T_bx: tuple = (-T_d / 2, T_d / 2)
+    T_by: tuple = (-T_d / 2, T_d / 2)
+    T_bz: tuple = (0, 140)
+    E_zr1: int = 50
+    E_zr2: int = 100
+    n_el: int = 64
+
+
+@dataclass
+class BallAnomaly:
+    x: Union[int, float]
+    y: Union[int, float]
+    z: Union[int, float]
+    d: Union[int, float]
+    perm: Union[int, float]
+
+
+@dataclass
+class Boundary:
+    x_0: Union[int, float] = 0
+    y_0: Union[int, float] = 0
+    z_0: Union[int, float] = 0
+    x_length: Union[int, float] = 32
+    y_length: Union[int, float] = 32
+    z_length: Union[int, float] = 32
+
+@dataclass
+class MeasurementInformation:
+    saline: tuple[float, str]
+    saline_height: tuple[float, str]
+    temperature: tuple[float, str]
+    timestamp: str
+
+
+@dataclass
+class CSVConvertInfo:
+    l_path: str
+    s_path: str
+    s_csv: str
+    n_samples: int
+
+
+@dataclass
+class HitBox:
+    r_min: Union[int, float]
+    r_max: Union[int, float]
+    x_min: Union[int, float]
+    x_max: Union[int, float]
+    y_min: Union[int, float]
+    y_max: Union[int, float]
+    z_min: Union[int, float]
+    z_max: Union[int, float]
